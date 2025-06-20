bd72b366b3cf3a6109f4b12fc54d896604473c26
Your Name <you@example.com>
2025-06-20 04:29:13 +0300

    fix: configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Calculator } from './components/calculator/calculator';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'calculator', component: Calculator },
+];
