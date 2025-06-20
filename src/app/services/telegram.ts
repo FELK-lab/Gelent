6e3613533ce3c7a533e3641de39e7604078813b9
Your Name <you@example.com>
2025-06-20 07:03:23 +0300

    feat: update profile page UI and navbar

--- src/app/services/telegram.ts
+++ src/app/services/telegram.ts
@@ -4,8 +4,9 @@ interface TgUser {
     id: number;
     first_name: string;
     last_name: string;
-    username: string;
+    username?: string;
     language_code: string;
+    photo_url?: string;
 }
 
 @Injectable({
